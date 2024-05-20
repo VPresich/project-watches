7332e07dd97b1083e1d8d6ff9d1b1c8074775f79
VPresich <valya.alexandrova.presich@gmail.com>
2024-05-20 06:50:56 +0200

    added bgr images for mobile menu

--- src/js/mobile-menu-handler.js
+++ src/js/mobile-menu-handler.js
@@ -1,9 +1,14 @@
+import { onScrollToElement } from './scroll';
 const mobileNavRef = document.querySelector('.js-mobile-nav');
 const mobileMenuRef = document.querySelector('.js-mobile-menu');
 
 mobileNavRef &&
   mobileMenuRef &&
-  mobileNavRef.addEventListener('click', (event) => {
+  mobileNavRef.addEventListener('click', event => {
+    event.preventDefault();
+    const item = event.target;
     if (!event.target.matches('a')) return;
+    const targetId = item.getAttribute('href').substring(1);
+    onScrollToElement(targetId);
     mobileMenuRef.classList.remove('is-open');
   });
